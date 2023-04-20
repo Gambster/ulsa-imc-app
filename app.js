de1c63163d92e374b03a9c6eca8e0fed596b6026
Gambster <gamboa040200@gmail.com>
2023-04-19 22:52:35 -0600

    Code refactor: app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express')
-const path = require('path')
 const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require("body-parser")
-const expressStatusMonitor = require('express-status-monitor');
 const router = require('./routes/router')
 
 const app = express()
@@ -13,9 +10,6 @@ app.use(expressLayouts)
 app.set('layout', './layouts/main.ejs')
 
 app.use(express.static(path.join(__dirname,'/')))
-
-// app.use(bodyParser.json({limit: '50mb'}));
-// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(router.routes)
 
